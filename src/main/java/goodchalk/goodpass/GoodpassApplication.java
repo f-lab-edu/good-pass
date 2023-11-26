6fac1cdf899674ae9336462687285a86a7341315
dgyim <yimdgkr@gmail.com>
2023-11-26 14:14:47 +0900

    create spring project

---
+++ src/main/java/goodchalk/goodpass/GoodpassApplication.java
@@ -0,0 +1,13 @@
+package goodchalk.goodpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodpassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodpassApplication.class, args);
+    }
+
+}
