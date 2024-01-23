f0e42b3d2a97fb95539915f0a559bfe76785b9a0
dgyim <yimdgkr@gmail.com>
2024-01-23 23:05:59 +0900

    운영과 개발 profile로 분리하기

--- src/test/java/com/goodchalk/goodpass/dailypass/domain/SignatureRepositoryIntegrationTest.java
+++ src/test/java/com/goodchalk/goodpass/dailypass/domain/SignatureRepositoryIntegrationTest.java
@@ -3,6 +3,7 @@ package com.goodchalk.goodpass.dailypass.domain;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -10,6 +11,7 @@ import java.io.InputStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class SignatureRepositoryIntegrationTest {
     @Autowired
     private SignatureRepository signatureRepository;
