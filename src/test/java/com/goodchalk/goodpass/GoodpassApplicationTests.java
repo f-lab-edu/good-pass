cf064298379b2cc8c155ba6c14372a87ddc9ad94
dgyim <yimdgkr@gmail.com>
2024-01-24 23:25:03 +0900

    간단한 코드 정리

--- src/test/java/com/goodchalk/goodpass/GoodpassApplicationTests.java
+++ src/test/java/com/goodchalk/goodpass/GoodpassApplicationTests.java
@@ -1,11 +1,7 @@
-package goodchalk.goodpass;
+package com.goodchalk.goodpass;
 
-import com.goodchalk.goodpass.DevConfig;
-import com.goodchalk.goodpass.GoodpassApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = GoodpassApplication.class)
