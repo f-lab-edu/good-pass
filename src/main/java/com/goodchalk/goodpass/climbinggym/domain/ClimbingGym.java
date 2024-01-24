1a03dd256f7f78072f4c1d403773e3dab63cabc1
dgyim <yimdgkr@gmail.com>
2024-01-24 23:51:44 +0900

    mysql, h2 호환 가능한 db 스키마와 엔터티 구성

--- src/main/java/com/goodchalk/goodpass/climbinggym/domain/ClimbingGym.java
+++ src/main/java/com/goodchalk/goodpass/climbinggym/domain/ClimbingGym.java
@@ -2,21 +2,29 @@ package com.goodchalk.goodpass.climbinggym.domain;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
-@Table("CLIMBING_GYM")
+@Table("climbing_gym")
 @Getter
 @NoArgsConstructor
 public class ClimbingGym {
     @Id
+    @Column("id")
     private Long id;
+    @Column("climbing_gym_name")
     private String climbingGymName;
+    @Column("address")
     private String address;
+    @Column("owner")
     private String owner;
+    @Column("contact")
     private String contact;
+    @Column("email")
     private String email;
+    @Column("poster_link")
     private String posterLink;
 
     @Builder
