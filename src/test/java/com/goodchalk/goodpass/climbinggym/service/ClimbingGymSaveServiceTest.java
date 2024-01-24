19e7461c9b508a088195ca2f72b98d48d42cacee
dgyim <yimdgkr@gmail.com>
2024-01-24 23:25:03 +0900

    간단한 코드 정리

--- src/test/java/com/goodchalk/goodpass/climbinggym/service/ClimbingGymSaveServiceTest.java
+++ src/test/java/com/goodchalk/goodpass/climbinggym/service/ClimbingGymSaveServiceTest.java
@@ -2,21 +2,19 @@ package com.goodchalk.goodpass.climbinggym.service;
 
 import com.goodchalk.goodpass.climbinggym.domain.ClimbingGym;
 import com.goodchalk.goodpass.climbinggym.domain.ClimbingGymRepository;
-import com.goodchalk.goodpass.climbinggym.service.dto.ClimbingGymCreator;
 import com.goodchalk.goodpass.climbinggym.domain.stub.ClimbingGymMemoryRepository;
-import org.assertj.core.api.Assertions;
+import com.goodchalk.goodpass.climbinggym.service.dto.ClimbingGymCreator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ClimbingGymSaveServiceTest {
     private final ClimbingGymRepository climbingGymRepository = new ClimbingGymMemoryRepository();
     private final ClimbingGymSaveService climbingGymSaveService = new ClimbingGymSaveService(climbingGymRepository);
 
-    //아래 코드 같은 경우네는 given when then이 분명하지 하지 않은데 이럴 때 어떻게 해야하는가?
-    //저장이 정상적으로 수행되었는지를 체크하려면 조회에 대한 기능을 같이 검증해야 한다...
-    //기능 검증이 완전히 분리가 안 되고 있다.
     @DisplayName("클라이밍장을 등록했을 때 등록된 클라이밍장이 정상적으로 조회되는가?")
     @Test
     void register() {
@@ -28,6 +26,6 @@ class ClimbingGymSaveServiceTest {
         Optional<ClimbingGym> climbingGymOptional = climbingGymRepository.findById(id);
         ClimbingGym climbingGym = climbingGymOptional.orElseThrow(RuntimeException::new);
 
-        Assertions.assertThat(climbingGym.getClimbingGymName()).isEqualTo("그래비티 클라이밍");
+        assertThat(climbingGym.getClimbingGymName()).isEqualTo("그래비티 클라이밍");
     }
 }
