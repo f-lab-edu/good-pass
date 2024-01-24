f8d4df3aa58222158439920a2ad59486c629d9e7
dgyim <yimdgkr@gmail.com>
2024-01-24 23:51:44 +0900

    mysql, h2 호환 가능한 db 스키마와 엔터티 구성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,13 @@
 DROP TABLE IF EXISTS climbing_gym;
 
 create table climbing_gym(
-    id int NOT NULL AUTO_INCREMENT,
-    climbing_gym_name varchar,
-    address varchar,
-    owner varchar,
-    contact varchar,
-    email varchar,
-    poster_link varchar,
+    id bigint NOT NULL AUTO_INCREMENT,
+    climbing_gym_name varchar(40),
+    address varchar(100),
+    owner varchar(15),
+    contact varchar(15),
+    email varchar(25),
+    poster_link varchar(100),
     CONSTRAINT CLIMBING_GYM_PK PRIMARY KEY(id)
 );
 
@@ -15,14 +15,14 @@ DROP TABLE IF EXISTS daily_pass;
 
 create table daily_pass
 (
-    id   int NOT NULL AUTO_INCREMENT,
-    climbing_gym_id int,
-    signature_status           varchar,
-    user_name           varchar,
-    contact             varchar,
-    daily_use_contract           varchar,
-    privacy_contract             varchar,
-    submit_date_time datetime,
-    request_date_time datetime,
+    id   bigint NOT NULL AUTO_INCREMENT,
+    climbing_gym_id bigint,
+    signature_status varchar(10),
+    user_name varchar(15),
+    contact varchar(15),
+    daily_use_contract varchar(6),
+    privacy_contract varchar(6),
+    submit_datetime datetime,
+    request_datetime datetime,
     CONSTRAINT DAILY_PASS_PK PRIMARY KEY (id)
 );
