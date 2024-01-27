3305cc33d1cb22e2eb79fb5326d969883fbb6860
dgyim <yimdgkr@gmail.com>
2024-01-27 16:05:30 +0900

    - 컨트롤러 통합테스트 진행 중 bug fix

--- src/main/java/com/goodchalk/goodpass/climbinggym/controller/ClimbingGymPosterSearchController.java
+++ src/main/java/com/goodchalk/goodpass/climbinggym/controller/ClimbingGymPosterSearchController.java
@@ -3,11 +3,11 @@ package com.goodchalk.goodpass.climbinggym.controller;
 import com.goodchalk.goodpass.climbinggym.controller.dto.response.ClimbingGymPosterDto;
 import com.goodchalk.goodpass.climbinggym.service.ClimbingGymPosterSearchService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class ClimbingGymPosterSearchController {
     private final ClimbingGymPosterSearchService climbingGymPosterSearchService;
